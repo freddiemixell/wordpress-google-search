5f4ecb78de26839e20324f3a598513ec1f793196
Freddie <fmixell@gmail.com>
2019-06-29 16:17:20 -0400

    initial commit

---
+++ plugin.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Google Site Search
+Description: Search your site using Google Algoritm. Pipe that data directly into Google Analytics.
+Author: Freddie Mixell
+Author URI: https://github.com/freddiemixell
+Version: 0.0.1
+*/

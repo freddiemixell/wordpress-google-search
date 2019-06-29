3c0d0028b1f54221324b7636b32041b8c7c1895b
freddiemixell <fmixell@gmail.com>
2019-06-29 19:44:02 -0400

    adding text domain for i18n

--- fm-google-site-search.php
+++ fm-google-site-search.php
@@ -2,6 +2,7 @@
 /*
 Plugin Name: Google Site Search
 Description: Search your site using Google Algoritm. Pipe that data directly into Google Analytics.
+Text Domain: fm-google-site-search
 Author: Freddie Mixell
 Author URI: https://github.com/freddiemixell
 Version: 0.0.1

0cd827f508c6d53bcee2fb4793a26de2ee1138cf
Freddie <fmixell@gmail.com>
2019-06-29 18:29:52 -0400

    created basic class and tested shortcode works

--- plugin.php
+++ plugin.php
@@ -6,3 +6,27 @@ Author: Freddie Mixell
 Author URI: https://github.com/freddiemixell
 Version: 0.0.1
 */
+
+$config = array(
+  'api_key' => 'AIzaSyDUb8Sq1NhQPxQCstoG_bzsuEVYah0BMDg',
+  'cx'      => '003422635988353438707:zn9yenb4_xq'
+);
+
+class GoogleCSE {
+  protected $api_key;
+  protected $cx;
+
+  public function __construct( $config )
+  {
+    $this->api_key = $config['api_key'];
+    $this->cx = $config['cx'];
+    add_shortcode('google_search', array($this, 'shortcode'));
+  }
+
+  public function shortcode()
+  {
+    return "This is working";
+  }
+};
+
+new GoogleCSE( $config );

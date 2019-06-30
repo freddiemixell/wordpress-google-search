0dad29b7cad70a612fc17ef4bed3e3559e20ac06
freddiemixell <fmixell@gmail.com>
2019-06-29 21:14:43 -0400

    activation/deactivation hooks

--- fm-google-site-search.php
+++ fm-google-site-search.php
@@ -18,23 +18,48 @@ if ( ! defined( 'ABSPATH' ) ) {
   die;
 }
 
-$textdomain = 'fm-google-site-search';
-
 $config = array(
-  'api_key' => 'AIzaSyDUb8Sq1NhQPxQCstoG_bzsuEVYah0BMDg',
-  'cx'      => '003422635988353438707:zn9yenb4_xq'
+  'api_key'    => 'AIzaSyDUb8Sq1NhQPxQCstoG_bzsuEVYah0BMDg',
+  'cx'         => '003422635988353438707:zn9yenb4_xq',
+  'textdomain' => 'fm-google-site-search',
 );
 
 class GoogleCSE {
-  protected $api_key;
-  protected $cx;
 
   public function __construct( $config )
   {
     $this->api_key = $config['api_key'];
     $this->cx = $config['cx'];
+    $this->textdomain = $config['textdomain'];
     add_shortcode('google_search', array($this, 'shortcode'));
     add_action('admin_menu', array($this, 'google_search_admin'));
+    register_activation_hook( __FILE__, array($this, 'activate_google_search'));
+    register_deactivation_hook( __FILE__, array($this, 'deactivate_google_search'));
+  }
+
+  public function activate_google_search()
+  {
+    global $wpdb;
+    $charset_collate = $wpdb->get_charset_collate();
+    $table_name = $wpdb->prefix . 'fm_google_site_search';
+  
+    $sql = "CREATE TABLE $table_name (
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      api_key varchar(50) NOT NULL DEFAULT '',
+      search_id varchar(50) NOT NULL DEFAULT '',
+      PRIMARY KEY  (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+  }
+
+  public function deactivate_google_search()
+  {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'fm_google_site_search';
+    $sql = "DROP TABLE IF EXISTS $table_name";
+    $wpdb->query($sql);
   }
 
   public function shortcode()
@@ -45,7 +70,7 @@ class GoogleCSE {
   public function google_search_admin()
   {
     add_menu_page(
-      __( 'Google CSE', $textdomain ),
+      __( 'Google CSE', $this->textdomain ),
       'Google CSE',
       'manage_options',
       'fm-google-site-search',
@@ -55,7 +80,7 @@ class GoogleCSE {
 
   public function render_admin_page()
   {
-    esc_html_e( 'Admin Page Test', $textdomain );
+    esc_html_e( 'Admin Page Test', $this->textdomain );
   }
 };
 

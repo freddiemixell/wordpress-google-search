8b83b1640a7f99157996129f7692081d918f8a38
freddiemixell <fmixell@gmail.com>
2019-07-01 12:24:52 -0400

    proof of concept

--- fm-google-site-search.php
+++ fm-google-site-search.php
@@ -25,27 +25,83 @@ if ( ! class_exists( 'GoogleCSE' ) ) {
 
     private $textdomain;
     private $options;
-  
+    private $results_url;
+
     public function __construct()
     {
       $this->textdomain = 'fm-google-site-search';
       $this->options = get_option( 'google-options' );
+      $this->results_url = 'google-search-results';
       add_shortcode( 'google_search', array( $this, 'shortcode' ) );
       add_action( 'admin_menu', array( $this, 'google_search_options_page' ) );
-      add_action( 'admin_init', array( $this, 'google_settings') );
+      add_action( 'admin_init', array( $this, 'google_settings' ) );
       add_action( 'admin_post_nopriv_google_search', array( $this, 'search_site' ) );
-      add_action( 'admin_post_google_search', array( $this, 'search_site') );
+      add_action( 'admin_post_google_search', array( $this, 'search_site' ) );
       add_action( 'query_vars', array( $this, 'set_query_var' ) );
       add_action( 'init', array( $this, 'custom_add_rewrite_rule' ), 10 );
       add_action( 'init', array( $this, 'google_search_flush_rewrite_rules_maybe' ), 20 );
       add_filter('template_include', array( $this, 'google_results_page' ) );
-      register_activation_hook( __FILE__, array( $this, 'flush_on_activate') );
+      register_activation_hook( __FILE__, array( $this, 'flush_on_activate' ) );
     }
-  
+
     public function search_site()
     {
-      status_header(200);
-      die("Server received '{$_POST['google-search-input']}' from your browser.");
+      $next_page;
+      $prev_page;
+      $query = $_POST['google-search-input'];
+      $base_url = 'https://www.googleapis.com/customsearch/v1';
+      $fields = 'items(title,link,displayLink,snippet),queries,searchInformation(formattedSearchTime,formattedTotalResults)';
+      global $wp_version;
+      $args = array(
+        'headers' => array(
+          'Referer' => site_url(),
+          'Accept-Encoding' => 'gzip',
+          'user-agent'  => 'WordPress/' . $wp_version . '; ' . home_url(),
+        )
+      );
+
+      $request = $base_url .
+        '?key=' . $this->options['api_key'] .
+        '&cx=' . $this->options['search_id'] .
+        '&q=' . $query .
+        '&fields=' . $fields;
+
+      $response = wp_remote_get( esc_url_raw( $request ), $args);
+
+      if ( is_array( $response ) ) {// array of http header lines
+        $api_response = json_decode( wp_remote_retrieve_body( $response ), true );
+
+        // Request is always present in response
+        // It is always an array with just one element
+        $request_arr = $api_response['queries']['request'][0];
+
+        // Next Page not present if the current results are the last page
+        if ( !empty( $api_response['queries']['nextPage'] ) && is_array( $api_response['queries']['nextPage'] ) ) {
+          $next_page = $api_response['queries']['nextPage'][0]['startIndex'];
+        }
+
+        // Previous Page not present if the current results are the first page
+        if ( !empty( $api_response['queries']['previousPage'] ) && is_array( $api_response['queries']['previousPage'] ) ) {
+          $prev_page = $api_response['queries']['previousPage'][0]['startIndex'];
+        }
+
+        $search_term = $request_arr['searchTerms'];
+        $count = $request_arr['count'];
+        $start_index = $request_arr['startIndex'];
+
+        // Search Information
+        $search_time = $api_response['searchInformation']['formattedSearchTime'];
+        $total_results = $api_response['searchInformation']['formattedTotalResults'];
+
+        // Search Result Items
+        $items = $api_response['items'];
+
+        require_once( __DIR__ . '/views/results_page.php' );
+        $url = site_url() . '/' . $this->results_url;
+        wp_redirect( $url );
+        die;
+      }
+
     }
   
     public function shortcode()
@@ -60,16 +116,14 @@ if ( ! class_exists( 'GoogleCSE' ) ) {
         ) {
         return;
       }
-  
-      $nonce_field = wp_nonce_field( 'fm_form_submit', 'fm_nonce', true, false );
-  
+
       $form = '<form action="' .  esc_url( admin_url( "admin-post.php" ) ) . '" method="post">';
       $form .= '<input type="hidden" name="action" value="google_search" />';
       $form .= '<input name="google-search-input" placeholder="' . __( 'Search...', $this->textdomain ) . '" required type="text" />';
       $form .= '<button class="fm-google-search-submit" id="google-cse-submit">' . __( 'Search', $this->textdomain ) . '</button>';
-      $form .= $nonce_field;
+      $form .= wp_nonce_field( 'fm_form_submit', 'fm_nonce', true, false );
       $form .= '</form>';
-  
+
       return $form;
     }
   
@@ -134,7 +188,7 @@ if ( ! class_exists( 'GoogleCSE' ) ) {
       );      
     }
   
-    public function sanatize( $input )
+    private function sanitize( $input )
     {
       $new_input = array();
       if( isset( $input['api_key'] ) ) {
@@ -148,16 +202,16 @@ if ( ! class_exists( 'GoogleCSE' ) ) {
       return $new_input;
     }
   
-    public function print_section_info()
+    private function print_section_info()
     {
       $google_cse_url = 'https://cse.google.com/cse/';
-      $google_api_url = 'https://code.google.com/apis/console/';
+      $google_api_url = 'https://developers.google.com/custom-search/v1/overview';
   
       echo '<p>' . esc_html__( 'Create Search Engine ID: ', $this->textdomain ) . '<a href="' . esc_url( $google_cse_url ) . '" target="_blank" rel="noopener noreferrer">' . esc_html__( $google_cse_url, $this->textdomain ) . '</a></p>';
       echo '<p>' . esc_html__( 'Create Google Custom Search v1 api key ', $this->textdomain ) . '<a href="' . esc_url( $google_api_url ) . '" target="_blank" rel="noopener noreferrer">' . esc_html__( $google_api_url, $this->textdomain ) . '</a></p>';
     }
   
-    public function api_key_callback()
+    private function api_key_callback()
     {
       printf(
         '<input type="text" id="api_key" name="google-options[api_key]" value="%s" />',
@@ -165,7 +219,7 @@ if ( ! class_exists( 'GoogleCSE' ) ) {
       );
     }
   
-    public function search_id_callback()
+    private function search_id_callback()
     {
       printf(
         '<input type="text" id="search_id" name="google-options[search_id]" value="%s" />',
@@ -175,13 +229,13 @@ if ( ! class_exists( 'GoogleCSE' ) ) {
 
     public function set_query_var( $vars )
     {
-      array_push($vars, 'results_page'); // ref url redirected to in add rewrite rule
+      $vars[] = 'results_page'; // ref url redirected to in add rewrite rule
       return $vars;
     }
 
     public function custom_add_rewrite_rule()
     {
-      add_rewrite_rule('^google-search-results$','index.php?results_page=1','top');
+      add_rewrite_rule('^' . $this->results_url . '$','index.php?results_page=1','top');
       flush_rewrite_rules();
     }
     

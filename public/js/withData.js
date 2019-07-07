4897d960078794ac364381dd6e431ecde56b303a
freddiemixell <fmixell@gmail.com>
2019-07-07 14:16:31 -0400

    removing unused code

--- public/js/withData.js
+++ public/js/withData.js
@@ -49,21 +49,6 @@ function withData(WrappedComponent) {
       .catch(() => this.setState({error: 2}))
     }
 
-    debounce(func, wait, immediate) {
-      var timeout;
-      return function() {
-        var context = this, args = arguments;
-        var later = function() {
-          timeout = null;
-          if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-      };
-    };
-
     render() {
       return <WrappedComponent {...this.state} />;
     }

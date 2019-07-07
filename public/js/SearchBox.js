124c52050f5d14bba147f370613f400729797d64
freddiemixell <fmixell@gmail.com>
2019-07-06 23:14:54 -0400

    converting form to styled components

--- public/js/SearchBox.js
+++ public/js/SearchBox.js
@@ -1,18 +1,41 @@
+import styled from 'styled-components';
+
+const Form = styled.form.attrs((props) => ({
+  role: "search",
+  method: "post",
+  className: "search-form",
+  onSubmit: props.searchSite
+}))`
+  padding-bottom: 20px;
+`;
+
+const SearchInput = styled.input.attrs((props) => ({
+  type:'search',
+  title:"Search for:",
+  className:"search-field",
+  name:"search-input",
+  defaultValue: props.query,
+  onChange: props.handleChange,
+}))``;
+
+const Submit = styled.input.attrs(() => ({
+  className: "search-field",
+  type: "submit",
+  value: "Search"
+}))`
+  margin-left: 5px;
+`;
+
 function SearchBox(props) {
 
-  const { query, handleChange, searchSite } = props;
-
   return (
-    <form onSubmit={searchSite}>
-      <input
-        type='text'
-        name="search-box"
-        placeholder="Searching..."
-        defaultValue={query}
-        onChange={handleChange}
-      />
-      <input type="submit" />
-    </form>
+    <Form {...props}>
+      <label>
+      <span className="screen-reader-text">Search for: </span>
+        <SearchInput {...props} />
+      </label>
+      <Submit />
+    </Form>
   )
           
 }

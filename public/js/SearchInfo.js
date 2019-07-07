618c2f0f00a397ff1f4b61a3a829feb77fded35e
freddiemixell <fmixell@gmail.com>
2019-07-06 23:46:06 -0400

    preventing page jumping

--- public/js/SearchInfo.js
+++ public/js/SearchInfo.js
@@ -8,9 +8,9 @@ function SearchInfo(props) {
     ? props.results.searchInformation.formattedTotalResults
     : '';
 
-  const results = totalResults ? `${totalResults} results` : '';
+  const results = totalResults ? `${totalResults} results` : '0 results';
 
-  const time = searchTime ? `(${searchTime} seconds)` : '';
+  const time = searchTime ? `(${searchTime} seconds)` : '(0 seconds)';
   
   return (
     <p>{results} {time}</p>
--- public/partials/fm-google-site-search-public-results.php
+++ public/partials/fm-google-site-search-public-results.php
@@ -1,1 +1,1 @@
-<div id="app"></div>
+<div id="app" style="min-height: 50vh;"></div>
